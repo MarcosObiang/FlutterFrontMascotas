15f8205e1928ffa47dfa634193dc926332ce6f9c
MarcosObiang <marcosobiang98@gmail.com>
2025-04-17 22:28:18 +0200

    nota añadida sobre la baseURL

--- lib/const_values/const_values.dart
+++ lib/const_values/const_values.dart
@@ -1,6 +1,12 @@
 class ConstValues {
+
+    /// Cambiar a vuestra url de ngrok la variable [baseUrl]
   static const String baseUrl =
       "https://2ea6-2a0c-5a81-3408-bd00-eff0-1b38-2f63-b931.ngrok-free.app";
+
+    ///
+    ///
+    ///
   static const String loginUrl = "$baseUrl/auth/login";
   static const String redirectUrl = "com.example.mascotascitas:/oauth2redirect";
   static const String googleClientId =
