799b23db98c1d8e8338fa0f00b2f65ad3ca31dcd
MarcosObiang <marcosobiang98@gmail.com>
2025-04-17 22:26:30 +0200

    modificada forma de autenticacion

--- lib/services/auth/WebLoginService.dart
+++ lib/services/auth/WebLoginService.dart
@@ -1,26 +1,90 @@
 import 'dart:developer' as developer;
+import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
+import 'package:mascotas_citas/Exceptions/ModuleException.dart';
+import 'package:mascotas_citas/Exceptions/TimeOutException.dart';
 import 'package:mascotas_citas/const_values/const_values.dart';
 import 'package:mascotas_citas/interfaces/auth/IAuthServices.dart';
 import 'package:app_links/app_links.dart';
+import 'package:mascotas_citas/services/ApiService.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class WebLoginService implements IAuthServices {
   final appLinks = AppLinks();
+  final FlutterWebAuth2 appAuth = FlutterWebAuth2();
+  ApiService apiService;
+
+  WebLoginService({required this.apiService});
+
   @override
   Future<Map<String, dynamic>?> login() async {
     try {
-      await launchUrlString(ConstValues.loginUrl);
-      return await _listenForRedirects();
+      final authorizationEndpoint =
+          Uri.parse('https://accounts.google.com/o/oauth2/v2/auth');
+
+      final scope = 'profile'; // Define los permisos que necesitas
+
+      final authorizationUrl = authorizationEndpoint.replace(queryParameters: {
+        'client_id': ConstValues.googleClientId,
+        'redirect_uri': ConstValues.redirectUrl,
+        'response_type': 'code',
+        'scope': scope,
+        // Puedes añadir un 'state' para seguridad
+      });
+      // Solicitar el código de autorización (solo abrirá la página en el navegador)
+      print(authorizationUrl.toString());
+      // Realizar la autorización
+      final result = await FlutterWebAuth2.authenticate(
+          url: authorizationUrl.toString(),
+          callbackUrlScheme: 'com.example.mascotascitas',
+          options: FlutterWebAuth2Options(useWebview: false));
+
+      // El flujo se detiene aquí, ya que solo abrimos el navegador y esperamos el código de autorización
+      // Después de la autorización, Google redirigirá al backend
+      if (result != null) {
+        print('Código de autorización recibido: ${result}');
+        String authCode = _getAuthCode(result);
+       return _getAccessToken(authCode);
+      } else {
+        print('Autorización fallida o cancelada');
+      }
     } catch (e) {
-      developer.log('Error en login: ${e.toString()}');
-      return null;
+      print('Error al iniciar sesión: $e');
+    }
+  }
+
+  String _getAuthCode(String uriString) {
+    Uri uri = Uri.parse(uriString);
+    String code = uri.queryParameters['code']!;
+    return code;
+  }
+
+  Future<Map<String, dynamic>> _getAccessToken(String authCode) async {
+    try {
+      final response =
+          await apiService.get(path: "/auth/google/code", queryParams: {
+        "code": authCode,
+      });
+
+      return {
+        'token': response.data['token'],
+        'refreshToken': response.data['refreshToken'],
+        'userUID': response.data['userUID'],
+        'expirationDate': response.data['expirationDate']
+      };
+
+      return response.data;
+    } on Exception catch (e) {
+      throw Exception(e);
     }
   }
 
   Future<Map<String, dynamic>?> _listenForRedirects() async {
     try {
-      // Escucha el stream de URIs
-      final uriStream = await appLinks.stringLinkStream.first;
+      final uriStream = await appLinks.stringLinkStream.first
+          .timeout(Duration(seconds: 30), onTimeout: () async {
+        throw TimeOutException(
+            message: "Tiempo de espera agotado", title: "Error", content: " ");
+      });
       return _extractAuthTokenFromUri(Uri.parse(uriStream));
     } catch (e) {
       developer.log('Error al escuchar redirección: ${e.toString()}');
