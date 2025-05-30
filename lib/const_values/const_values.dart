18079756e4f8ebe3dd61e622e00088f6e4304a10
Your Name <you@example.com>
2025-05-30 16:16:41 +0200

    merge

--- lib/const_values/const_values.dart
+++ lib/const_values/const_values.dart
@@ -2,7 +2,7 @@ class ConstValues {
   /// Cambiar a vuestra host de ngrok la variable [hostName] (la url sin el https://)
 
   static const String hostName =
-      "a0ae-85-61-96-56.ngrok-free.app";
+      "4fb9-85-61-96-56.ngrok-free.app";
 
 
 
